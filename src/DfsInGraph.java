5d8ab09f313b76be23b8b4d2fcdf73cf88a5f1f7
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-30 20:43:41 -0700

    coded the recursive method that will take care of the calls for the next consecutive vertex's calls

--- src/DfsInGraph.java
+++ src/DfsInGraph.java
@@ -4,6 +4,23 @@ import java.util.*;
 
 public class DfsInGraph {
 
+
+    static void dfs(int current, ArrayList<ArrayList<Integer>> adj, ArrayList<Integer>ans, int[]visited){
+        ans.add(current);
+
+        for(int i= 0; i< adj.get(current).size(); i++){
+            int nextConnection = adj.get(current).get(i);
+
+            if(visited[nextConnection] ==0){
+                visited[nextConnection] =1;
+                dfs(nextConnection, adj, ans, visited);
+            }
+
+
+        }
+
+
+    }
 public static ArrayList<Integer> dfsInGraphs(int vertices, ArrayList<ArrayList<Integer>> adj){
     ArrayList<Integer> ans = new ArrayList<>();
 
