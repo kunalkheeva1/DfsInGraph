629dbad4d0ad00514709510c47e2c447d3bf0e31
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-30 20:37:33 -0700

    created a main method for the functioning of the algorithm, Now going to code actual recursive method for its implementation in main

--- src/DfsInGraph.java
+++ src/DfsInGraph.java
@@ -1,7 +1,21 @@
+import java.util.*;
+
+
+
 public class DfsInGraph {
 
+public static ArrayList<Integer> dfsInGraphs(int vertices, ArrayList<ArrayList<Integer>> adj){
+    ArrayList<Integer> ans = new ArrayList<>();
 
-    
+    int [] visited = new int[vertices];
+    for(int i =0; i<vertices; i++){
+        visited[i] =0;
+    }
+    visited[0] =1;
+    dfs(0, adj, ans, visited);
+    return ans;
+}
+
 
 
     public static void main(String[] args) {
